d0d60c373ce8348e4e5e53746b6d3f18bb1b003e
ktrn <katarina.lusicic@gmail.com>
2021-01-21 14:52:53 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+    <div id="app">
+        <nav id="nav" class="navbar navbar-expand-lg navbar-light">
+            <!-- Just an image -->
+
+        <a class="navbar-brand" href="#">
+        <img src="@/assets/fipu_logo.png" alt="">
+        </a>
+
+        <button class="navbar-toggler" 
+        type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarToggler" 
+        aria-controls="navbarToggler" 
+        aria-expanded="false" 
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarToggler">
+
+        <form id="search" class="navbar-form form-inline ml-auto">
+            <input v-model="store.searchTerm" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        </form>
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+            <router-link to="/" class="nav-link">Home</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link to="/login" class="nav-link">Login</router-link>
+            </li>
+            <li class="nav-item">
+            <router-link to="/signup" class="nav-link">Sign up</router-link>
+            </li>
+        </ul>
+        </div>
+        </nav>
+
+
+        <div class="container">
+            <router-view />
+            <!-- tu router dinamicki ubaci screen -->
+        </div>
+    </div>
+</template>
+
+<script>
+import store from '@/store';
+
+export default {
+    name: 'app',
+    data() {
+        return{
+            store,
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+#nav {
+    padding: 30px;
+    background-color: #fff !important;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="row">
+
+    <div class="col-8">      
+        <instagram-card v-for="card in filteredCards" :key="card.url" :info="card"/>
+    </div>
+
+    <div class="col-4">
+        Sidebar
+    </div>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import InstagramCard from '@/components/InstagramCard.vue';
+import store from '@/store';
+
+let cards = [];
+
+cards = [
+    {url: 'https://picsum.photos/id/1/400/400', description: 'laptop', time: 'few minutes ago...'},
+    {url: 'https://picsum.photos/id/2/400/400', description: 'laptop #2', time: 'an hour ago...'},
+    {url: 'https://picsum.photos/id/3/400/400', description: 'laptop #3', time: 'few hours ago...'},
+];
+
+let hello = 'world';
+
+export default {
+    name: 'home',
+    data: function(){
+        return{
+            cards: cards,
+            store,
+        }
+    },
+    computed: {
+        filteredCards() {
+            let termin = this.store.searchTerm;
+
+            this.cards.filter(card => card.description.includes(termin));     
+        },
+    },
+    components: {
+        InstagramCard,
+    },
+};
+</script>
+        
